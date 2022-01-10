5cd34540a5cf2f1706f434726e35f566a219f7b2
THUSHARA <sivaharithushra98@gmail.com>
2022-01-10 11:45:43 +0530

    First Commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,14 +1,70 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'model/file.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<HomeScreen> {
+  late Future<List<Data>> futureData;
+
+  @override
+  void initState() {
+    super.initState();
+    futureData = fetchData();
+  }
+
+  Future<List<Data>> fetchData() async {
+    final response =
+        await http.get('https://jsonplaceholder.typicode.com/albums');
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((data) => new Data.fromJson(data)).toList();
+    } else {
+      throw Exception('Unexpected error occured!');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("List View"),
-        centerTitle: true,
+    return MaterialApp(
+      title: 'Flutter API and ListView Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter ListView'),
+        ),
+        body: Center(
+          child: FutureBuilder<List<Data>>(
+            future: futureData,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Data> data = snapshot.requireData;
+
+                return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        height: 75,
+                        color: Colors.white,
+                        child: Center(
+                          child: Text(data[index].title),
+                        ),
+                      );
+                    });
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              // By default show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
       ),
     );
   }
---
+++ lib/model/file.dart
@@ -0,0 +1,15 @@
+class Data {
+  final int userId;
+  final int id;
+  final String title;
+
+  Data({required this.userId, required this.id, required this.title});
+
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
